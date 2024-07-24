1f30fc1fc9883beab00cf54d9a38fd44b93e9c3c
Oscar Figueroa <fisocodes@gmail.com>
2024-07-24 11:04:18 -0700

    Implemented resend email verification

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,14 +1,22 @@
-import { Get, Controller, Query, Post, Body } from '@nestjs/common'
+import { Get, Controller, Query, Post, Body, ConflictException } from '@nestjs/common'
 import { ApiTags } from '@nestjs/swagger'
 import { User } from '@prisma/client'
 import { AuthService } from './auth.service'
 import { SignInDto } from './dto/signin.dto'
+import { UsersService } from '../users/users.service'
+import { NodemailerService } from '../nodemailer.service'
+import { JwtService } from '@nestjs/jwt'
 
 @ApiTags( 'auth' )
 @Controller( 'auth' )
 export class AuthController
 {
-    constructor( private authService: AuthService ) {}
+    constructor(
+        private authService: AuthService,
+        private usersService: UsersService,
+        private nodemailerService: NodemailerService,
+        private jwtService: JwtService
+    ) {}
 
     @Get( '/verify-email' )
     async verifyEmail(
@@ -16,6 +24,23 @@ export class AuthController
     ): Promise<User>
     {
         return this.authService.verifyEmail( emailVerificationToken )
+    }
+
+    @Post( 'resend-email-verification' )
+    async resendEmailVerification(
+        @Query( 'idOrEmailOrUsername' ) idOrEmailOrUsername: string
+    ): Promise<User>
+    {
+        const foundUser = await this.usersService.readOne( idOrEmailOrUsername )
+
+        if( foundUser.isEmailVerified )
+            throw new ConflictException( 'Email is already verified' )
+
+        const emailVerificationToken = await this.jwtService.signAsync( { sub: foundUser.id } )
+
+        await this.nodemailerService.sendVerificationEmail( foundUser.email, emailVerificationToken )
+
+        return foundUser
     }
 
     @Post( '/sign-in' )
